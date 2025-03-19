64bc39ab5023cda65959193db79e98ad6cf1d2c6
Sadiul Hakim <sadiulhakim@gmail.com>
2025-03-19 15:53:29 +0600

    Made PWA

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'quran-cache-v1';
+const urlsToCache = [
+    '/images/icon-192x192.png',
+    '/images/icon-512x512.png',
+    '/images/screenshot1.png',
+    '/js/manifest.json'
+];
+
+// Install service worker and cache static assets
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(urlsToCache))
+    );
+});
+
+// Serve assets from cache when offline
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((cachedResponse) => {
+                return cachedResponse || fetch(event.request);
+            })
+    );
+});
+
+// Clear old caches on service worker activation
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
